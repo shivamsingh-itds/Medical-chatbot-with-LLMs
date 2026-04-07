2853c96c5dc37e666e62311acd2cfba4d2f3310c
shivamsingh-itds <shivam.itds@gmail.com>
2026-04-07 20:08:26 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="shivam singh",
+    author_email="shivam.itds@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
